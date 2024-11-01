07fe8e5ee54e4d86412736790d2bc5cead1990f4
benalevine32 <Benalevine32@gmail.com>
2024-11-01 13:01:40 -0400

    More 2D array work

--- UdemyCourse/2DArray.py
+++ UdemyCourse/2DArray.py
@@ -1,4 +1,24 @@
 import numpy as np
 
-twoDemensionalArray = np.array([[1,2,3], [4,5,6,], [7,8,9]])
+twoDemensionalArray = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12],[13,14,15, 16]])
 print(twoDemensionalArray)
+print(" ")
+
+#Adding Column to two dimensional array
+
+#newTwoDemensionalArray = np.insert(twoDemensionalArray, 0, [[17,18,19,20]], axis=1)
+#print(newTwoDemensionalArray)
+#print(" ")
+
+#Accessing any element
+#a[i][j] i = row / j = column
+
+def accessElements(array, rowIndex, columnIndex):
+    if rowIndex >= len(array) or columnIndex >= len(array[0]):
+        print("Incorrect Index")
+    else:
+        print(array[rowIndex][columnIndex])
+
+
+
+accessElements(twoDemensionalArray, 1, 2)
